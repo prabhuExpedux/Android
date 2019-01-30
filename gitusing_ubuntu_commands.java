4f846d480b225698051a140b874efe87c71e4061
prabhuExpedux <prabhu.v@expedux.com>
2019-01-30 15:11:21 +0530

    this second time to upload a file in gitgitusing_ubuntu_commands.java

---
+++ gitusing_ubuntu_commands.java
@@ -0,0 +1,11 @@
+gee@suresh-gee-expedux:~/git_WorkSpace$ git status
+fatal: not a git repository (or any of the parent directories): .git
+gee@suresh-gee-expedux:~/git_WorkSpace$ ls
+Android  gitusing_ubuntu_commands.java
+gee@suresh-gee-expedux:~/git_WorkSpace$ cd Adroid
+bash: cd: Adroid: No such file or directory
+gee@suresh-gee-expedux:~/git_WorkSpace$ cd Android
+gee@suresh-gee-expedux:~/git_WorkSpace/Android$ ls
+LICENSE  README.md
+gee@suresh-gee-expedux:~/git_WorkSpace/Android$ touch gitusing_ubuntu_commands.java
+gee@suresh-gee-expedux:~/git_WorkSpace/Android$ notepad gitusing_ubuntu_commands.java
